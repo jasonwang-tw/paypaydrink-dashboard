ea33150d6212a558b9ac7ef53f4b8bf43f96a735
JasonWang <ff1986755087@gmail.com>
2021-01-22 00:32:49 +0800

    topmenu commit

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/jasonwang-tw/paypaydrink.git master:gh-pages
+git push -f https://github.com/jasonwang-tw/paypaydrink-dashboard.git master:gh-pages
 
 cd -
--- src/components/topmenu.vue
+++ src/components/topmenu.vue
@@ -1,45 +1,34 @@
 <template>
   <div class="header py-3 px-5 border-b border-sup2-500 top-0 sticky z-20 bg-white">
     <div class="container m-auto flex justify-between">
-      <div class="logo">
-        <router-link to="/">
-          <img src="../assets/logo.svg" alt="" />
-        </router-link>
-      </div>
-      <div
-        class="nav md:flex fixed bg-white h-full md:h-auto md:relative duration-200 top-0 pt-5 px-5 md:p-0 shadow-xl md:shadow-none"
-        :class="{ mobileNavActive: mobileClick }"
-      >
-        <div
-          class="block md:hidden mb-10 rounded-3xl border-sup3-500 border inline-block px-1.5 py-0.5"
-          @click="mobileClick = !mobileClick"
-        >
-          <i class="pay-close1 text-sup3-500"></i>
+      <div class="nav-left flex items-center">
+        <div class="logo mr-5">
+          <router-link to="/">
+            <img src="../assets/logo.svg" alt="" />
+          </router-link>
         </div>
-        <ul class="mainMenu md:flex items-center list-none pl-0 text-right">
-          <li
-            v-for="item in mainPage"
-            :key="item"
-            class="text-main-500 hover:text-sup1-100 px-4 md:py-0 py-2"
-          >
-            <router-link :to="item.link" class="no-underline">{{ item.name }}</router-link>
-          </li>
-        </ul>
-        <div class="mobileMenu flex items-center" @click="mobileClick = !mobileClick">
+        <div class="searchBar flex items-center">
+          <i class="pay-search text-xl mr-5"></i>
+          <input class="" type="text" />
+        </div>
+      </div>
+      <div class="nav-right flex items-center">
+        <div class="nav-right-items shopCart">
+          <i class="pay-cart text-xl mr-5"></i>
+          <span>545,448</span>
+        </div>
+        <div class="nav-right-items notice border-l border-r border-sup2-500">
+          <i class="pay-notice text-xl"></i>
+        </div>
+        <div class="nav-right-items menuBtn" @click="mobileClick = !mobileClick">
           <i class="pay-menu"></i>
         </div>
-        <!-- <ul
-          class="dashboardMenu md:flex items-center md:border-l pl-0 list-none border-t pt-3 mt-3 md:border-t-0 md:pt-0 md:mt-0"
-        >
-          <li
-            v-for="item in dashboard"
-            :key="item"
-            class="text-sub-500 px-4 hover:text-sub-100 flex md:py-0 py-2 justify-end"
-          >
-            <div v-html="item.icon" class="mr-3 duration-200 "></div>
-            <router-link :to="item.link" class="no-underline">{{ item.name }}</router-link>
-          </li>
-        </ul> -->
+      </div>
+      <div
+        class="sidebar-nav h-full fixed bg-red-100 right-0 top-0 duration-200"
+        :class="{ mobileNavActive: mobileClick }"
+      >
+        <h1>54584</h1>
       </div>
     </div>
   </div>
@@ -50,24 +39,24 @@
     name: 'topmenu',
     data() {
       return {
-        // mainPage: [
-        //   {
-        //     link: '/',
-        //     name: '優惠活動'
-        //   },
-        //   {
-        //     link: '/',
-        //     name: '飲料故事'
-        //   },
-        //   {
-        //     link: '/',
-        //     name: '魔法秘訣'
-        //   },
-        //   {
-        //     link: '/',
-        //     name: '立即體驗'
-        //   }
-        // ],
+        mainPage: [
+          {
+            link: '/',
+            name: '優惠活動'
+          },
+          {
+            link: '/',
+            name: '飲料故事'
+          },
+          {
+            link: '/',
+            name: '魔法秘訣'
+          },
+          {
+            link: '/',
+            name: '立即體驗'
+          }
+        ],
         // dashboard: [
         //   {
         //     link: '/',
@@ -88,13 +77,18 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  @media (max-width: 768px) {
-    .nav {
-      width: 250px;
-      right: -250px;
-    }
-    .mobileNavActive {
-      right: 0;
-    }
+  [class^='pay-'] {
+    color: var(--color-main-100);
+  }
+  .nav-right-items {
+    margin: 0 0.5rem;
+    padding: 0 1.25rem;
+  }
+  .sidebar-nav {
+    width: 250px;
+    right: -250px;
+  }
+  .mobileNavActive {
+    right: 0;
   }
 </style>
