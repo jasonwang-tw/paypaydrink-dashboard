f3437cf3d737cdfade14b3523d0e084be14091a7
JasonWang <ff1986755087@gmail.com>
2021-01-24 00:48:06 +0800

    topmenu complate

--- src/components/topmenu.vue
+++ src/components/topmenu.vue
@@ -13,54 +13,119 @@
         </div>
       </div>
       <div class="nav-right flex items-center">
-        <div class="nav-right-items shopCart">
-          <i class="pay-cart text-xl mr-5"></i>
-          <span>545,448</span>
+        <!-- miniCart -->
+        <div
+          class="nav-right-items shopCart"
+          @mouseenter="miniCartHidden = false"
+          @mouseleave="miniCartHidden = true"
+        >
+          <i class="pay-cart text-xl"></i>
+          <!-- miniCartList -->
+          <div
+            class="shopCartList absolute bg-white border border-sup2-500 p-5 mt-2.5 -ml-6 rounded-lg duration-200"
+            :class="{ listHidden: miniCartHidden }"
+          >
+            <ul class="list-none pl-0 text-main-500">
+              <li v-for="i in 4" class="">
+                <router-link to="/" class="flex mb-5 no-underline">
+                  <div class="productImg bg-white rounded-lg overflow-hidden mr-5 flex-shrink-0">
+                    <img src="../../src/assets/product.jpg" alt="" />
+                  </div>
+                  <div class="productInfo flex justify-between">
+                    <div class="overflow-hidden">
+                      <div class="etc mr-5">
+                        productNameproductNameproductName
+                      </div>
+                      <span class="text-sub-500">$ 99</span>
+                    </div>
+                    <span class="flex-shrink-0">x 91</span>
+                  </div>
+                </router-link>
+              </li>
+            </ul>
+            <p class="text-sup3-500 text-center">還有其他 5 個項目</p>
+            <router-link to="/" class="btn-dark-blue block text-center mt-5"
+              >查看購物車</router-link
+            >
+          </div>
         </div>
-        <div class="nav-right-items notice border-l border-r border-sup2-500">
+        <!-- notice -->
+        <div
+          class="nav-right-items notice border-l border-r border-sup2-500"
+          @mouseenter="miniNoticeHidden = false"
+          @mouseleave="miniNoticeHidden = true"
+        >
           <i class="pay-notice text-xl"></i>
+          <!-- noticeList -->
+          <div
+            class="noticeList absolute bg-white border border-sup2-500 p-5 mt-2.5 -ml-6 rounded-lg duration-200"
+            :class="{ listHidden: miniNoticeHidden }"
+          >
+            <ul class="list-none pl-0 text-main-500">
+              <li v-for="i in 4" class="">
+                <router-link to="/" class="flex mb-5 no-underline">
+                  <div class="msgInfo">
+                    <div class="etc">
+                      付款已確認
+                    </div>
+                    <div class="etc text-sup1-900">
+                      訂單<span class="text-sub-500">200718NQDUVMMW</span
+                      >的付款已確認，店鋪備料中，請依照訂單
+                    </div>
+                    <small class="text-sup3-500">2021-01-22 12:00</small>
+                  </div>
+                </router-link>
+              </li>
+            </ul>
+            <p class="text-sup3-500 text-center">還有其他 5 個項目</p>
+            <router-link to="/" class="btn-dark-blue block text-center mt-5"
+              >查看全部通知</router-link
+            >
+          </div>
         </div>
-        <div class="nav-right-items menuBtn" @click="switchBtn">
+        <div class="nav-right-items menuBtn cursor-pointer" @click="menuhBtn">
           <i class="pay-menu"></i>
         </div>
       </div>
+      <!-- mask -->
       <div
         class="fixed top-0 left-0 w-full h-full bg-gray-900 opacity-50 duration-200"
         :class="{ mask: maskOff }"
+        @click="menuhBtn"
       ></div>
+      <!-- sidebarNav -->
       <div
-        class="sidebarNav h-full fixed bg-white shadow-lg right-0 top-0 duration-200 flex justify-center pt-10"
+        class="sidebarNav h-full fixed bg-white shadow-lg right-0 top-0 duration-200 flex"
         :class="{ mobileNav: mobileStatus }"
       >
-        <div class="flex flex-wrap justify-center content-between">
+        <div class="flex flex-wrap content-between pt-10 px-10">
           <div class="sidebarNavTop">
-            <div class="close" @click="switchBtn">
-              <i class="pay-close-outline"></i>
-            </div>
-            <div class="account flex">
-              <div class="userImg overflow-hidden rounded-full">
-                <img src="../../src/assets/drink1.png" alt="" />
+            <router-link to="/" class="account flex items-center no-underline">
+              <div class="userImg overflow-hidden rounded-full bg-white mr-5">
+                <img src="../../src/assets/how.jpg" alt="" />
               </div>
-              <div class="userInfo">
-                <div class="userName">Jasowang</div>
-                <router-link to="/"><small>帳戶資訊</small></router-link>
+              <div>
+                <div class="userName text-main-500">howhow</div>
+                <small class="text-sub-500">帳戶資訊</small>
               </div>
-            </div>
-            <div class="payCoin">
+            </router-link>
+            <div class="payCoin mt-5 border-b pb-5">
               <small>PayPayPoint</small><br />
-              <span>999,999</span>
+              <span class="text-sub-500 font-bold">999,999</span>
             </div>
-            <ul class="list-none pl-0">
-              <li v-for="(item,index) in mainPage">
-                <router-link :to="item.link" class="no-underline">{{item.name}}</router-link>
+            <ul class="list-none pl-0 mt-5">
+              <li v-for="(item, index) in mainPage" class="text-main-500 mb-5 hover:text-sup1-100">
+                <router-link :to="item.link" class="no-underline">{{ item.name }}</router-link>
               </li>
             </ul>
           </div>
-          <div class="sidebarNavBottom">
-            <p>立即下載 配配飲</p>
-            <span>客製屬於您的飲品</span>
-            <img src="../../src/assets/appDown-col.png" alt="" />
-            <img src="../../src/assets/downhand.svg" alt="" />
+          <div class="sidebarNavBottom text-main-500 text-center">
+            <div class="text-2xl font-medium mb-2">
+              立即下載 <span class="text-sub-500">配配飲</span>
+            </div>
+            <span class="mb-5 block">客製屬於您的飲品</span>
+            <img class="my-auto inline-block mb-10" src="../../src/assets/appDown-col.png" alt="" />
+            <img class="my-auto inline-block w-40" src="../../src/assets/downhand.svg" alt="" />
           </div>
         </div>
       </div>
@@ -72,7 +137,7 @@
   export default {
     name: 'topmenu',
     methods: {
-      switchBtn: function() {
+      menuhBtn: function() {
         this.mobileStatus = !this.mobileStatus
         this.maskOff = !this.maskOff
       }
@@ -101,7 +166,9 @@
             name: '登出'
           }
         ],
-        mobileStatus: true,
+        mobileStatus: false,
+        miniCartHidden: true,
+        miniNoticeHidden: true,
         maskOff: true
       }
     }
@@ -111,6 +178,36 @@
 <style scoped lang="scss">
   [class^='pay-'] {
     color: var(--color-main-100);
+  }
+  .shopCart:before {
+    content: '99';
+    color: white;
+    border-radius: 99px;
+    padding: 5px 5px;
+    line-height: 0.5rem;
+    font-size: 0.5rem;
+    display: inline-block;
+    transform: translate(7px, 5px);
+    background-color: var(--color-sub);
+  }
+  .shopCartList,
+  .noticeList {
+    width: 300px;
+  }
+  .listHidden {
+    opacity: 0;
+    visibility: hidden;
+    margin-top: 1.5rem;
+  }
+  .notice:before {
+    content: '';
+    width: 5px;
+    height: 5px;
+    display: inline-block;
+    position: absolute;
+    border-radius: 20px;
+    transform: translate(-5px, 2px);
+    background-color: var(--color-sub);
   }
   .userImg {
     width: 50px;
@@ -122,12 +219,11 @@
     }
   }
   .nav-right-items {
-    margin: 0 0.5rem;
-    padding: 0 1.25rem;
+    padding: 0 1.5rem;
   }
   .sidebarNav {
-    width: 250px;
-    right: -250px;
+    width: 300px;
+    right: -300px;
   }
   .mobileNav {
     right: 0;
@@ -135,5 +231,20 @@
   .mask {
     opacity: 0;
     visibility: hidden;
+  }
+  .productInfo {
+    width: calc(100% - (50px + 1.25rem));
+  }
+  .msgInfo {
+    width: 100%;
+  }
+  .productImg {
+    width: 50px;
+    height: 50px;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
   }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="home">
     <topmenu />
+    <div class="container mx-auto">
+      <div class="flex items-center justify-center mt-10">
+        <div class="waiter mr-5">
+          <img src="../../src/assets/waiter.png" alt="" />
+        </div>
+        <div class="hello text-4xl font-bold text-main-500">
+          您好 Jason，<br />請問今天想喝點什麼?
+        </div>
+      </div>
+      <ul class="mainDrink list-none grid grid-cols-4 gap-5">
+        <li v-for="(item, index) in mainDrink">
+          <router-link :to="item.link" class="no-underline block text-center bg-gray-600 rounded-lg">{{ item.name }}</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,8 +26,42 @@
     name: 'home',
     components: {
       topmenu
+    },
+    data() {
+      return {
+        mainDrink: [
+          {
+            link: '/',
+            name: '紅茶'
+          },
+          {
+            link: '/',
+            name: '綠茶'
+          },
+          {
+            link: '/',
+            name: '水果茶'
+          },
+          {
+            link: '/',
+            name: '鮮奶茶'
+          },
+          {
+            link: '/',
+            name: '抹茶'
+          },
+          {
+            link: '/',
+            name: '珍珠奶茶'
+          }
+        ]
+      }
     }
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .hello {
+    line-height: 3rem;
+  }
+</style>
